cc22c62f8fe4ba71569996752f5d1d30aaea4ec5
= <rtyenriques@gmail.com>
2022-03-01 16:06:37 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todo';
 
 class App extends Component {
 
@@ -17,11 +18,20 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    // this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.dispatch(addTodo(this.state.todo)); 
     this.setState({ todo: '' });
   }
 
+  // addTodo = () => {
+  //   return ({
+  //     type: 'ADD_TODO',
+  //     todo: this.state.todo
+  //   })
+  // }
+
   render() {
+    debugger
     const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
     return (
       <div className="App">
@@ -47,4 +57,13 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: (todo) => {
+      dispatch(addTodo(todo))
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
